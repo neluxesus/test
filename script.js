a2a51c84eb0508222b9e8c1ece8ff6e72a19a2a7
Armonter <150815976+panteramc@users.noreply.github.com>
2024-04-08 14:48:02 +0100

    Update script.js

--- script.js
+++ script.js
@@ -215,22 +215,8 @@ hamburger.addEventListener("click", () => {
     navbarLinks.classList.toggle("active");
 })
 
-/*FAQs*/
-const accordionItemHeaders = document.querySelectorAll(".accordion-item-header");
 
-accordionItemHeaders.forEach(accordionItemHeader => {
-    accordionItemHeader.addEventListener("click", () => {
-        accordionItemHeader.classList.toggle("active");
-        const accordionItemBody = accordionItemHeader.nextElementSibling;
 
-        if(accordionItemHeader.classList.contains("active")) accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
-        else accordionItemBody.style.maxHeight = "0px";
-    });
-});
-
-/*Config navbar*/
-const serverName = document.querySelector(".server-name");
-const serverLogo = document.querySelector(".logo-img");
 /*Config header*/
 const serverIp = document.querySelector(".minecraft-server-ip");
 const serverLogoHeader = document.querySelector(".logo-img-header");
